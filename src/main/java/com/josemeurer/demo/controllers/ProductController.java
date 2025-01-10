00f5e0bf3b9a62b794d78390b1ae22c2aef9e3a7
Jose Meurer <josewmeurer@gmail.com>
2025-01-10 20:11:00 -0300

    findById no controller e servicer, junto com lombok builder para construir o dto

--- src/main/java/com/josemeurer/demo/controllers/ProductController.java
+++ src/main/java/com/josemeurer/demo/controllers/ProductController.java
@@ -2,9 +2,11 @@ package com.josemeurer.demo.controllers;
 
 import com.josemeurer.demo.dtos.ProductDto;
 import com.josemeurer.demo.services.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,20 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private HttpServletRequest servlet;
+
     @GetMapping
     public ResponseEntity<List<ProductDto>> findAll() {
         List<ProductDto> list = productService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> findById(@PathVariable("id") long id) {
+        ProductDto dto = productService.findById(id);
+
+
+        return ResponseEntity.ok().body(dto);
     }
 }
--- src/main/java/com/josemeurer/demo/services/ProductService.java
+++ src/main/java/com/josemeurer/demo/services/ProductService.java
@@ -4,7 +4,9 @@ import com.josemeurer.demo.dtos.ProductDto;
 import com.josemeurer.demo.entities.Product;
 import com.josemeurer.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,7 +21,21 @@ public class ProductService {
         return list.stream().map(this::entityToDto).toList();
     }
 
+    public ProductDto findById(long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
+
+        return entityToDto(product);
+    }
+
+
+
     private ProductDto entityToDto(Product entity) {
-        return new ProductDto(entity.getId(), entity.getName(), entity.getDescription(), entity.getPrice());
+        return ProductDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .description(entity.getDescription())
+                .price(entity.getPrice())
+                .build();
     }
 }
